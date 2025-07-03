36b06c8b29c8a1ae869e16482151c18369e0ff83
belmonteken <belmonteken@gmail.com>
2025-07-03 10:18:00 +0000

    minimal changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase Studio Demo',
+      title: 'Flutter Firebase Studio',
       theme: ThemeData(
         // This is the theme of your application.
         //
